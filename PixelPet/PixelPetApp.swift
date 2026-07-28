7f53c1c913575b654558d8676851b3964f0955fb
Rtals <sungmin1170@gmail.com>
2026-07-28 16:02:46 +0900

    Initial commit

---
+++ PixelPet/PixelPetApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelPetApp.swift
+//  PixelPet
+//
+//  Created by 성민 on 7/28/26.
+//
+
+import SwiftUI
+
+@main
+struct PixelPetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
